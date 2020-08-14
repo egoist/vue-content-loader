f3055f562fc6f24fdfa829d5cf0b5e09b0f66621
Paolo Manaloto <manalotopaolof@gmail.com>
2020-08-14 21:09:55 +0800

    Updated loader design of instagram

--- src/InstagramLoader.js
+++ src/InstagramLoader.js
@@ -6,11 +6,15 @@ export default {
   render(h, { data }) {
     return (
       <ContentLoader {...data} height={480}>
-        <circle cx="30" cy="30" r="30" />
+        <circle cx="20" cy="32" r="10.5" />
 
-        <rect x="75" y="13" rx="4" ry="4" width="100" height="13" />
-        <rect x="75" y="37" rx="4" ry="4" width="50" height="8" />
-        <rect x="0" y="70" rx="5" ry="5" width="400" height="400" />
+        <rect x="40" y="27" rx="2" ry="2" width="140" height="10" />
+        <rect x="0" y="60" rx="2" ry="2" width="400" height="400" />
+        <rect x="0" y="470" rx="2" ry="2" width="140" height="10" />
+        <rect x="0" y="485" rx="2" ry="2" width="250" height="10" />
+        <rect x="0" y="500" rx="2" ry="2" width="110" height="10" />
+        <rect x="0" y="515" rx="2" ry="2" width="180" height="10" />
+        <rect x="0" y="530" rx="2" ry="2" width="170" height="10" />
       </ContentLoader>
     )
   }
