bcd143bdda31a82b9d5299c100a3e5b100bd3fd5
Benoît Burgener <benoit.burgener@gmail.com>
2021-01-18 19:16:21 +0100

    test: setup unit tests suite using Jest

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        // Jest tests are run in Node and it requires commonjs modules
+        modules: process.env.NODE_ENV === 'test' ? 'commonjs' : false,
+        loose: true
+      }
+    ]
+  ],
+  plugins: ['@vue/babel-plugin-jsx']
+}
