e629eb1d5b2f9b64f02b7e1bd3d1b60caecd5105
EGOIST <0x142857@gmail.com>
2021-01-18 23:13:25 +0800

    remove test

--- src/index.test.js
+++
@@ -1,7 +0,0 @@
-import { mount } from 'vue-test-utils'
-import ContentLoader from './'
-
-test('it works', () => {
-  const wrapper = mount(ContentLoader)
-  expect(wrapper.isVueInstance()).toBe(true)
-})
