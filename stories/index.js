1a6fe754d91ee9dfff137622ad4ba16a7d4e8797
Benoît Burgener <benoit.burgener@gmail.com>
2021-01-27 09:49:52 +0100

    fix: comply to width & height attributes
    fix #46
    
    BREAKING CHANGE:
    
    `width` and `height` attributes are now applied as-is to the `svg` element, if you want the previous, responsive behavior, use only the new `viewBox` prop instead.
    
    Before: `width="200" height="100"`
    After: `viewBox="0 0 200 100"`

--- stories/index.js
+++ stories/index.js
@@ -135,7 +135,26 @@ section
       render() {
         return (
           <Container>
-            <ContentLoader width={400} height={150} />
+            <ContentLoader width={200} height={100} />
+          </Container>
+        )
+      }
+    }
+  })
+  .addStory({
+    title: 'custom viewBox',
+    component: {
+      render() {
+        return (
+          <Container>
+            <ContentLoader viewBox="0 0 250 110">
+              <rect x="0" y="0" rx="3" ry="3" width="250" height="10" />
+              <rect x="20" y="20" rx="3" ry="3" width="220" height="10" />
+              <rect x="20" y="40" rx="3" ry="3" width="170" height="10" />
+              <rect x="0" y="60" rx="3" ry="3" width="250" height="10" />
+              <rect x="20" y="80" rx="3" ry="3" width="200" height="10" />
+              <rect x="20" y="100" rx="3" ry="3" width="80" height="10" />
+            </ContentLoader>
           </Container>
         )
       }
